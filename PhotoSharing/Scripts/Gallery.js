682c4466d99cd77f9c6fb44aaac0e24185e39b4d
Mulugeta <muler.et@gmail.com>
2017-11-25 18:08:13 +0300

    Improved thumbnail view and layout, Added image gallery browser

---
+++ PhotoSharing/Scripts/Gallery.js
@@ -0,0 +1,27 @@
+﻿function openModal() {
+    document.getElementById('galleryModal').style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('galleryModal').style.display = "none";
+}
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var sources = $(".image-box");
+    if (n < 0) { n = 0; }
+    if (n > sources.length - 1) { n = sources.length - 1; }
+    var next = $(sources[n]);
+    var slide = $(".gallery-slide img");
+    slide.attr("src", next.find("a").attr("href"));
+}
